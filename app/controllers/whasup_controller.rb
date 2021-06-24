85889e9bc24b2ba16a13ed7374499798dd29fe9f
benjaminrdickson <bdickson8619@gmail.com>
2021-06-24 14:18:07 -0500

    adds 'http' gem

--- app/controllers/whasup_controller.rb
+++ app/controllers/whasup_controller.rb
@@ -1,6 +1,11 @@
 class WhasupController < ApplicationController
 
   def index
-    render json: {message: "Whaddup Dani!"}
+
+    response = HTTP.get(https://tenor.com/view/lets-drink-wha-whasup-gif-13166077)
+    render json: response.parse(:json)
+
+
+    # render json: {message: "Whaddup Dani!"}
   end
 end
